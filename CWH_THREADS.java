97ee49744a5551d1ed740a4efaba34b84cb330ba
Tanisha yadav <160209610+thisideTanisha@users.noreply.github.com>
2024-12-01 10:12:14 +0530

    Add files via upload

--- CWH_THREADS.java
+++ CWH_THREADS.java
@@ -2,25 +2,45 @@ package com.company;
 class MyThread1 extends Thread{
    @Override
     public void run(){
-       while(true){
+//       int i=0;
+//       while(i<10){
        System.out.println("My Cooking thread is runnimg ");
            System.out.println("I am happy");
-    }
+//           try {
+//               Thread.sleep(4);
+//           } catch (InterruptedException e) {
+//               throw new RuntimeException(e);
+//           }
+//    }
        }
 }
 class MyThread2 extends Thread{
     @Override
     public void run(){
-        while(true){
-            System.out.println(" Thread2 for chatting with her ");
-            System.out.println("I am sad");
-        }
+//        int i=0;
+//        while(i<10){
+//            System.out.println(" Thread2 for chatting with her (impppppppppp)");
+//            System.out.println("I am sad");
+//            try {
+//                Thread.sleep(4);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
     }
 }
 public class CWH_THREADS {
     public static void main(String[] args) {
         MyThread1 t1 = new MyThread1();
         MyThread2 t2 = new MyThread2();
+
+//        t1.setPriority(9);
+//        t2.setPriority(4);
+        System.out.println(t2.getPriority());
+        System.out.println(t1.getPriority());
+        System.out.println(t2.getState());
+        System.out.println(t1.getState());
+        System.out.println(Thread.currentThread().getState());
         t1.start();
         t2.start();
 
